7204f48a6e0343f3fb961878502a049579cf39e4
ReneTorres05 <53118876+ReneTorres05@users.noreply.github.com>
2019-07-20 19:59:38 -0500

    Juego tic

---
+++ juegotic2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegotic;
+
+
+import javax.swing.JButton;
+/**
+ *
+ * @author Rene
+ */
+public class juegotic2 {
+    JButton A;
+    int B;
+    juegotic2()
+    {
+        A=new JButton();
+        B=0;
+    }    
+}
+
